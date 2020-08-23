eac43602a9fa02ae54053723c6c0e6e7fd534387
arongan90 <arongan90@gmail.com>
2020-08-23 20:55:31 +0900

    여러개의 Input 관리, useRef로 DOM 선택하기

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,19 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
   const [text, setText] = useState('');
-  const onChange = e => {
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: '',
+  });
+
+  const textChange = e => {
     setText(e.target.value);
   };
-  const reset = () => {
-    setText('');
+
+  const { name, nickname } = inputs;
+  const nameInput = useRef();
+
+  const onChange = e => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
   };
+  const onReset = () => {
+    setText('');
+    setInputs({
+      name: '',
+      nickname: '',
+    });
+    nameInput.current.focus();
+  };
+
   return (
-    <div>
-      <input onChange={onChange} value={text} />
-      <button onClick={reset}>초기화</button>
-      <b>값 : </b> {text}
-    </div>
+    <>
+      <div>
+        <input onChange={textChange} value={text} ref={nameInput} />
+        <button onClick={onReset}>초기화</button>
+        <b>값 : </b> {text}
+      </div>
+      <div>
+        <input
+          name="name"
+          onChange={onChange}
+          value={name}
+          placeholder="이름"
+        />
+        <input
+          name="nickname"
+          onChange={onChange}
+          value={nickname}
+          placeholder="닉네임"
+        />
+        <button onClick={onReset}>초기화</button>
+        <b>{name}</b> : ({nickname})
+      </div>
+    </>
   );
 }
 
